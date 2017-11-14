8887cb4dde7a6121ac1e469ba880ee80d548cb5b
AndrewWaller <awalldrew2@gmail.com>
2017-11-14 00:14:48 -0800

    trying to host because of local issues

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ mongoose.connect(config.database);
 
 // On Connection
 mongoose.connection.on('connected', () => {
-    console.log('Connected to database '+config.database);
+  console.log('Connected to database '+config.database);
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-    console.log('Database Error: '+err);
+  console.log('Database error: '+err);
 });
 
 const app = express();
@@ -26,23 +26,29 @@ const users = require('./routes/users');
 // Port Number
 const port = 3000;
 
-//CORS Middleware
+// CORS Middleware
 app.use(cors());
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Body Parser Midleware
+// Body Parser Middleware
 app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.use('/users', users);
 
 // Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid Endpoint');
+  res.send('Invalid Endpoint');
 });
 
 // Start Server
 app.listen(port, () => {
-    console.log('Server started on port '+port);
+  console.log('Server started on port '+port);
 });
