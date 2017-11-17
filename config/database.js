41a814c203bcdda2607d2a3093ef20d8223b530c
AndrewWaller <awalldrew2@gmail.com>
2017-11-17 06:31:44 -0800

    setting up mlab for mongodb#2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://adrw4:12345678@ds040877.mlab.com:40877/adrw4', // 'mongodb://localhost:27017/nodeauth',
+  database: 'mongodb://adrw4:adrw4@ds040877.mlab.com:40877/adrw4', // 'mongodb://localhost:27017/nodeauth',
   secret: 'yoursecret'
 }
